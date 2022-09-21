0c9bf27164fc3c0f2659fb90850475eeaa34504c
BigTurtle8 <69373245+BigTurtle8@users.noreply.github.com>
2022-09-21 11:55:54 -0500

    Add files via upload

--- MovableSetting.java
+++ MovableSetting.java
@@ -1,16 +1,18 @@
 import mayflower.*;
 
-// WIP
+// still WIP
 
 // a collection of MovableSettingActors
 // so they move and don't move at the same time
 public class MovableSetting
 {
     private MovableSettingActor[] settingActors;
+    private int speed = 4;
     
     public MovableSetting()
     {
         this.settingActors = new MovableSettingActor[0];
+        speed = 4;
     }
     
     public void add(MovableSettingActor actor)
@@ -26,19 +28,30 @@ public class MovableSetting
         settingActors = temp;
     }
     
+    // KEY_LEFT = 37
+    // KEY_RIGHT = 39
     public void act()
     {
-        for (MovableSettingActor a : settingActors)
-        {
-            if (a.isBlocked())
-            {
-                return;
-            }
-        }
+        int tempDist = speed;
         
-        for (MovableSettingActor a : settingActors)
+        for (int i = 0; i < settingActors.length; i++)
         {
+            MovableSettingActor a = settingActors[i];
             
+            int dir = a.moveWithSetting(tempDist);
+            if (a.isBlocking())
+            {
+                // how far ALREADY MOVED setting needs to adjust
+                int temp = a.findClosestLegalPosition(tempDist, dir);
+                
+                // sets already moved settingactors to correct position
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    settingActors[j].correctMove(temp);
+                }
+                
+                tempDist = tempDist - Math.abs(temp);
+            }
         }
     }
 }
--- MovableSettingActor.java
+++ MovableSettingActor.java
@@ -2,7 +2,7 @@ import mayflower.*;
 
 // moonwalk bug
 
-public class MovableSettingActor extends Actor
+public abstract class MovableSettingActor extends Actor
 {
     public MovableSettingActor()
     {
@@ -11,18 +11,59 @@ public class MovableSettingActor extends Actor
     
     public void act()
     {
+        // move() is real act(), called in MovableSetting
+    }
+    
+    // called in MovableSetting
+    // returns direction
+    public int moveWithSetting(int speed)
+    {
         if (Mayflower.isKeyDown(Keyboard.KEY_RIGHT)) 
         {
-            setLocation(getX() - 4, getY());
+            setLocation(getX() - speed, getY());
+            return Keyboard.KEY_RIGHT;
         }
         else if (Mayflower.isKeyDown(Keyboard.KEY_LEFT))
         {
-            setLocation(getX() + 4, getY());
+            setLocation(getX() + speed, getY());
+            return Keyboard.KEY_LEFT;
         }
+        
+        return -1;
     }
     
-    public boolean isBlocked()
+    // when a setting object is blocked
+    // receive correct legal position and 
+    // move that much
+    public void correctMove(int dist)
     {
-        return false;
+        setLocation(dist, getY());
     }
+    
+    // if isBlocking(), then correct to closest legal position
+    public int findClosestLegalPosition(int currentSpeed, int dir)
+    {
+        // final dist moved in opposite direction
+        int distMoved = 0;
+            
+        if (dir == Keyboard.KEY_LEFT)
+        {
+            while (isBlocking()) 
+            {
+                distMoved--;
+                setLocation(getX() - 1, getY());
+            }
+        } else if (dir == Keyboard.KEY_RIGHT)
+        {
+            while (isBlocking()) 
+            {
+                distMoved++;
+                setLocation(getX() + 1, getY());
+            }
+        }
+        
+        return distMoved;
+    }
+    
+    public abstract boolean isBlocking();
 }
