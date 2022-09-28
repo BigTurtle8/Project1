24ad972f05cd51a73df9089b248e98204596fc5a
AndrewWang1234 <113624616+AndrewWang1234@users.noreply.github.com>
2022-09-28 14:46:58 -0500

    Add files via upload

--- TitleWorld.java
+++ TitleWorld.java
@@ -1,9 +1,17 @@
 import mayflower.*;
 
+/**
+ * @Andrew Wang
+ * 
+ * Creates the title World
+ */
 public class TitleWorld extends GameWorld
 {
     public TitleWorld(int s , int l)
     {
+        /**
+         * Sets the screen with the number of lives, score, introductory text, and title screen*/
+        
         super(s,l);
         setBackground("img/BG/BG.png");
         showText("Welcome to the game, press enter to begin",100, 300, Color.BLACK);
