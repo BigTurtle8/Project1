8572d25cd4b50edd3f00718507601eb460ca6e4d
AndrewWang1234 <113624616+AndrewWang1234@users.noreply.github.com>
2022-09-28 11:04:22 -0500

    Add files via upload

--- GameWorld.java
+++ GameWorld.java
@@ -11,11 +11,14 @@ public class GameWorld extends World
     private String currentLevel;
     private Boolean hasWon;
     private int score;
+    private int lives;
     /**
      * Constructor for objects of class GameWorld
      */
-    public GameWorld(){
+    public GameWorld(int s, int l){
         currentLevel = "title";
+        score = s;
+        lives = l;
     }
 
     public String getCurrentLevel() {
@@ -33,26 +36,54 @@ public class GameWorld extends World
     public void changehasWon(Boolean a) {
         hasWon = a;
     }
+    
+    public void changeScore(int amount) {
+        score = score + amount;
+    }
+    
+    public int getScore() {
+        return score;
+    }
+    
+    public int getLives() {
+        return lives;
+    }
+    
+    public void changeLives(int amount){
+        lives = lives + amount;
+    }
+    
+    public void updateText() {
+        showText("Score: " + score + " Lives: " + lives, 10, 30, Color.BLACK);
+    }
 
     public void act() {
+        
+        
         if(Mayflower.isKeyDown(Keyboard.KEY_ENTER))
         {
             if (getCurrentLevel().equals("title"))
             {
-                Mayflower.setWorld(new LevelOneWorld());
+                Mayflower.setWorld(new LevelOneWorld(getScore(), getLives()));
             }
             else if (getCurrentLevel().equals("levelOne"))
             {
-                Mayflower.setWorld(new LevelTwoWorld());
+                Mayflower.setWorld(new LevelTwoWorld(getScore(), getLives()));
             }
             else if (getCurrentLevel().equals("levelTwo"))
             {
-                Mayflower.setWorld(new LevelThreeWorld());
+                Mayflower.setWorld(new LevelThreeWorld(getScore(), getLives()));
             }
             else if(getCurrentLevel().equals("levelThree")) {
-                Mayflower.setWorld(new WinWorld());
+                Mayflower.setWorld(new WinWorld(getScore(), getLives()));
             }
-        }        
+        }
+        if(getCurrentLevel().equals("title")) {
+            changeScore(5);
+            changeLives(-1);
+        }
+        updateText();
+        
     }
 
 }
