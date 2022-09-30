62465b824efa2a228de71558bfbdddfafc57b963
AndrewWang1234 <113624616+AndrewWang1234@users.noreply.github.com>
2022-09-30 12:14:47 -0500

    Add files via upload

---
+++ Coin.java
@@ -0,0 +1,31 @@
+import mayflower.*;
+
+/**
+ * @Andrew Wang
+ *
+ * Creates a coin that interacts with the player
+ */
+public class Coin extends Consumable
+{
+    /**
+     * Constructor for objects of class heart
+     */
+    
+    public Coin()
+    {
+        MayflowerImage coin = new MayflowerImage("img/trap/coin.png");
+        coin.scale(50,50);
+        setImage(coin);
+    }
+
+    /**
+     *Increases the players lives by one if the player touches the heart
+    */
+    public void effect()
+    {
+            
+        //p.setLocation(350,0);
+        GameWorld w = (GameWorld) (getWorld());
+        w.changeScore(10);
+    }
+}
---
+++ Heart.java
@@ -0,0 +1,31 @@
+import mayflower.*;
+
+/**
+ * @Andrew Wang
+ *
+ * Creates a heart that interacts with the player
+ */
+public class Heart extends Consumable
+{
+    /**
+     * Constructor for objects of class heart
+     */
+    
+    public Heart()
+    {
+        MayflowerImage heart = new MayflowerImage("img/trap/heart.png");
+        heart.scale(50,50);
+        setImage(heart);
+    }
+
+    /**
+     *Increases the players lives by one if the player touches the heart
+    */
+    public void effect()
+    {
+                    
+        //p.setLocation(350,0);
+        GameWorld w = (GameWorld) (getWorld());
+        w.changeLives(1);
+    }
+}
