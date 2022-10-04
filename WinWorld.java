bb2482c11494ba00e82f6767e21ccdb4d028c898
AndrewWang1234 <113624616+AndrewWang1234@users.noreply.github.com>
2022-10-04 11:55:52 -0500

    Add files via upload

--- WinWorld.java
+++ WinWorld.java
@@ -8,19 +8,32 @@ import mayflower.*;
 
 public class WinWorld extends GameWorld
 {
+    /**
+     * Creates the screen with lives and score displayed
+     */
     public WinWorld(int s, int l)
     {
         /**
          * Sets the screen with the number of lives, score, and winning background
          */
         super(s, l);
-        setBackground("img/BG/Untitled Drawing (1).png");
+        setBackground("img/BG/WIN.png");
         super.changeCurrentLevel("WIN");
         showText("Your Score: " + super.getScore() + " Your lives: " + super.getLives(),100, 250, Color.BLACK);
+        if(super.getLives() > 1) {
+        showText(super.getScore() + "*" + super.getLives(),75, 450, Color.BLACK);
+        showText("Final Score: " + super.getScore()*super.getLives(),50, 450, Color.BLACK);
+        showText("You won! Press enter to play again.",25, 300, Color.BLACK);
+    }
+        else {
         showText("You won! Press enter to play again.",100, 300, Color.BLACK);
+    }
         super.changeHasLost(false);
     }
     
+    /**
+     * Resets the world
+     */
     public void resetWorld()
     {
         Mayflower.setWorld(new WinWorld(getScore(), getLives()));
