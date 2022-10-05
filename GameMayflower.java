fafcbc04edb6164c78aeb0f84cd16be1bd85019c
BigTurtle8 <69373245+BigTurtle8@users.noreply.github.com>
2022-10-04 22:34:05 -0500

    Added comments

--- GameMayflower.java
+++ GameMayflower.java
@@ -3,24 +3,27 @@ import mayflower.*;
 /**
  * @Andrew Wang
  * 
- * Creates the World 
+ * Creates the window for the world.
  */
 public class GameMayflower extends Mayflower
 {
-    
+    /**
+      * Constructor, creates the window for game.
+      */
     public GameMayflower()
     {
         super("Project 1", 800, 600);
     }
     
+    /**
+      * Sets the screen to the title world.
+      */
     public void init()
     {
-        /**
-         * Sets the screen to the title world
-        */
+        
         Mayflower.setFullScreen(false);
         World w = new TitleWorld(0, 3);
         Mayflower.setWorld(w);
     }
     
-    }
+}
